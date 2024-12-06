95feb4419198750b27bfbf817d23622dfd4d37d0
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-12-06 16:24:14 +0700

    test gcp

--- config/db.go
+++ config/db.go
@@ -10,7 +10,7 @@ var MongoString string = os.Getenv("MONGOSTRING")
 
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
-	DBName:   "restoran",
+	DBName:   "restaurant",
 }
 
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
