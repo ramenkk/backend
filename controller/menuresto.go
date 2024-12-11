68fd3069e2f89d35aad67962b8161d8ad0b83c6b
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-12-11 22:28:50 +0700

    add post resto

--- controller/menuresto.go
+++ controller/menuresto.go
@@ -1,13 +1,18 @@
 package controller
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+
 	"github.com/gocroot/config"
 	"github.com/gocroot/helper"
 	"github.com/gocroot/helper/atdb"
 	"github.com/gocroot/model"
 	"github.com/whatsauth/itmodel"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetRestaurant(respw http.ResponseWriter, req *http.Request) {
@@ -19,4 +24,22 @@ func GetRestaurant(respw http.ResponseWriter, req *http.Request) {
 		return
 	}
 	helper.WriteJSON(respw, http.StatusOK, resto)
+}
+func PostRestaurant(respw http.ResponseWriter, req *http.Request) {
+
+	var restoran model.Restaurant
+	if err := json.NewDecoder(req.Body).Decode(&restoran); err != nil {
+		helper.WriteJSON(respw, http.StatusBadRequest, itmodel.Response{Response: err.Error()})
+		return
+	}
+
+	result, err := config.Mongoconn.Collection("menu_makanan").InsertOne(context.Background(), restoran)
+	if err != nil {
+		helper.WriteJSON(respw, http.StatusInternalServerError, itmodel.Response{Response: err.Error()})
+		return
+	}
+
+	insertedID := result.InsertedID.(primitive.ObjectID)
+
+	helper.WriteJSON(respw, http.StatusOK, itmodel.Response{Response: fmt.Sprintf("Menu resto berhasil disimpan dengan ID: %s", insertedID.Hex())})
 }
