503035f34b8c5f5a3f5b076491d1839609b788a8
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2025-01-02 20:20:36 +0700

    mod struck add barcode

--- model/ramen.go
+++ model/ramen.go
@@ -9,6 +9,7 @@ type Outlet struct {
     Nama       string             `bson:"nama_outlet" json:"nama_outlet"`
     Alamat     string             `bson:"alamat" json:"alamat"`
     KodeOutlet string             `bson:"kode_outlet" json:"kode_outlet"`
+    Barcode    string             `bson:"barcode" json:"barcode"`
 }
 
 type Menu struct {
@@ -21,13 +22,16 @@ type Menu struct {
 }
 
 type Pesanan struct {
-    ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-    NamaPelanggan string            `bson:"nama_pelanggan" json:"nama_pelanggan"`
-    KodeOutlet    string            `bson:"kode_outlet" json:"kode_outlet"`
-    DaftarMenu    []ItemPesanan     `bson:"daftar_menu" json:"daftar_menu"`
-    TotalHarga    float64           `bson:"total_harga" json:"total_harga"`
-    StatusPesanan string            `bson:"status_pesanan" json:"status_pesanan"` 
-    TanggalPesanan primitive.DateTime `bson:"tanggal_pesanan" json:"tanggal_pesanan"`
+    ID               primitive.ObjectID    `bson:"_id,omitempty" json:"id,omitempty"`
+    NamaPelanggan    string                `bson:"nama_pelanggan" json:"nama_pelanggan"`
+    KodeOutlet       string                `bson:"kode_outlet" json:"kode_outlet"` 
+    DaftarMenu       []ItemPesanan         `bson:"daftar_menu" json:"daftar_menu"`
+    TotalHarga       float64               `bson:"total_harga" json:"total_harga"`
+    StatusPesanan    string                `bson:"status_pesanan" json:"status_pesanan"`
+    TanggalPesanan   primitive.DateTime    `bson:"tanggal_pesanan" json:"tanggal_pesanan"`
+    Pembayaran       string                `bson:"pembayaran" json:"pembayaran"` 
+    AdminID          primitive.ObjectID    `bson:"admin_id" json:"admin_id"` 
+    WaktuTerima      primitive.DateTime    `bson:"waktu_terima" json:"waktu_terima"` 
 }
 
 type ItemPesanan struct {
