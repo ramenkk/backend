470df2d52585a82f41e91895cadefee53addd37a
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2025-01-02 16:47:03 +0700

    mod struck

--- model/menuresto.go
+++ model/menuresto.go
@@ -4,14 +4,36 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Restaurant struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Nama      string             `bson:"nama,omitempty" json:"nama,omitempty"`
-	Kategori  string             `bson:"kategori,omitempty" json:"kategori,omitempty"`
-	Alamat    string             `bson:"alamat,omitempty" json:"alamat,omitempty"`
-	Telepon   string             `bson:"telepon,omitempty" json:"telepon,omitempty"`
-	Status    string             `bson:"status,omitempty" json:"status,omitempty"`
-	Waktubuka string             `bson:"waktubuka,omitempty" json:"waktubuka,omitempty"`
-	Harga     string             `bson:"harga,omitempty" json:"harga,omitempty"`
-	Photo     string             `bson:"photo,omitempty" json:"photo,omitempty"`
+type Outlet struct {
+    ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    Nama       string             `bson:"nama_outlet" json:"nama_outlet"`
+    Alamat     string             `bson:"alamat" json:"alamat"`
+    KodeOutlet string             `bson:"kode_outlet" json:"kode_outlet"`
+}
+
+type Menu struct {
+    ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    NamaMenu  string             `bson:"nama_menu" json:"nama_menu"`
+    Harga     float64            `bson:"harga" json:"harga"`
+    Deskripsi string             `bson:"deskripsi" json:"deskripsi"`
+    Gambar    string             `bson:"gambar" json:"gambar"`
+    OutletID  primitive.ObjectID `bson:"outlet_id" json:"outlet_id"`
+}
+
+type Pesanan struct {
+    ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+    NamaPelanggan string            `bson:"nama_pelanggan" json:"nama_pelanggan"`
+    KodeOutlet    string            `bson:"kode_outlet" json:"kode_outlet"`
+    DaftarMenu    []ItemPesanan     `bson:"daftar_menu" json:"daftar_menu"`
+    TotalHarga    float64           `bson:"total_harga" json:"total_harga"`
+    StatusPesanan string            `bson:"status_pesanan" json:"status_pesanan"` 
+    TanggalPesanan primitive.DateTime `bson:"tanggal_pesanan" json:"tanggal_pesanan"`
+}
+
+type ItemPesanan struct {
+    MenuID    primitive.ObjectID `bson:"menu_id" json:"menu_id"`
+    NamaMenu  string             `bson:"nama_menu" json:"nama_menu"`
+    Jumlah    int                `bson:"jumlah" json:"jumlah"`
+    HargaSatuan float64          `bson:"harga_satuan" json:"harga_satuan"`
+    Subtotal  float64            `bson:"subtotal" json:"subtotal"`
 }
