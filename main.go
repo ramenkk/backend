e456b47a886255fe98f4be058b7aa96b3e6fd17d
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2025-02-02 21:32:23 +0700

    mod main.go

--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package gocroot
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gocroot/middleware"
 	"github.com/gocroot/route"
-	"github.com/gorilla/csrf"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	
+
+	
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Default port jika tidak ada di env
-	}
-
-	// Middleware CSRF Protection
-	csrfMiddleware := csrf.Protect(
-		[]byte("your-secret-key"), // Ganti dengan key yang aman
-		csrf.Secure(true),         // Aktifkan hanya jika menggunakan HTTPS
-		csrf.HttpOnly(true),       // Cookie hanya bisa diakses oleh server
-		csrf.Path("/"),            // Berlaku untuk semua endpoint
-	)
-
-	// Gunakan middleware CSRF & Auth pada semua request
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", route.URL)
-
-	// Jalankan server dengan middleware
-	fmt.Println("Server berjalan di port", port)
-	log.Fatal(http.ListenAndServe(":"+port, csrfMiddleware(middleware.AuthMiddleware(mux))))
+func init() {
+	functions.HTTP("WebHook", route.URL)
+	
 }
+
